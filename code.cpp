26bc437b0fb4977bce6e8627da06ed82cc860435
be1001014rocks <prashant010696@gmail.com>
2017-05-24 22:28:35 +0530

    Add files via upload

--- code.cpp
+++ code.cpp
@@ -8,8 +8,8 @@ typedef long long int LL;
 typedef pair<int, int> PII;
 typedef vector<int> VI;
 typedef vector< PII > VPII;
-const int DIM = 50;
-int N = 50;
+const int DIM = 4;
+int N = 2;
 int id = 0;
 struct kdtree {
 	kdtree *left, *right;
@@ -67,35 +67,42 @@ kdtree* dynamic_build(kdtree *rootaddr, kdtree* p, int depth) {
 		rootaddr->right = NULL;
 	}
 	else {
-		id = depth % DIM;
+ 		id = depth % DIM;
+ 		//cout << depth << endl;
 		if (p->di[id] < rootaddr->di[id])
-			dynamic_build(rootaddr->left, p, depth + 1);
+			rootaddr->left = dynamic_build(rootaddr->left, p, depth + 1);
 		else
-			dynamic_build(rootaddr->right, p, depth + 1);
+			rootaddr->right= dynamic_build(rootaddr->right, p, depth + 1);
 	}
 	return rootaddr;
 }
 set<pair<int, kdtree*> >s;
-void nearest_neighbour(kdtree *rootaddr, kdtree* &best_node, int &best_dist, kdtree* p, int depth) {
+void nearest_neighbour(kdtree* rootaddr, kdtree* &best_node, int &best_dist, kdtree* p, int depth) {
 	if (rootaddr == NULL)
 		return;
-	int dis = distance(p, best_node);
+	int dis = distance(p,rootaddr);
+	//cout << dis << "0000" << endl;
 	if (dis < best_dist) {
-		best_dist = distance(p, best_node);
+		best_dist = distance(p, rootaddr);
 		best_node = rootaddr;
 	}
-	s.insert(make_pair(dis, rootaddr));
+	//cout << best_dist << endl;
+ 	s.insert(make_pair(dis, rootaddr));
 	id = depth % DIM;
 	if (p->di[id] < rootaddr->di[id]) {
 		nearest_neighbour(rootaddr->left, best_node, best_dist, p, depth + 1);
 		if (abs(rootaddr->di[id] - p->di[id]) < best_dist || s.size() < N)
 			nearest_neighbour(rootaddr->right, best_node, best_dist, p, depth + 1);
 	}
+		nearest_neighbour(rootaddr->left, best_node, best_dist, p, depth + 1);
+		nearest_neighbour(rootaddr->right, best_node, best_dist, p, depth + 1);
+	//cout << rootaddr->di[0] << endl;
 }
 void print(kdtree *nodeaddr, int depth, char c) {
 	if (nodeaddr == NULL)
 		return;
-	//cout << nodeaddr->x << " " << nodeaddr->y << " " << depth << " " << c << "\n";
+	//for(int i = 0 ; i < DIM; i ++){
+	//cout << nodeaddr->di[i] << " "  << " " << depth << " " << c << "\n";}
 	print(nodeaddr->left, depth + 1, 'l');
 	print(nodeaddr->right, depth + 1, 'r');
 }
@@ -112,31 +119,42 @@ int main() {
 	freopen("input.txt", "r", stdin);
 	int blk = 500;
 	int Q;
-	kdtree* rootaddr;
-	cin >> Q;
+	kdtree* rootaddr= NULL;
+	cin >> Q >> N;
+	//cout << Q << endl;
 	vector<kdtree*> vec;
+	kdtree* xy = (kdtree*)(malloc(sizeof(kdtree)));
 	for (int i = 0 ; i < Q; i ++) {
-		cout << "Enter the point" << endl;
-		kdtree* xy;
+		//cout << "Enter the point" << endl;
 		int x;
 		for (int j = 0; j < DIM; j++) {
 			cin >> x;
 			xy->di[j] = x;
 		}
-		cout << "Enter the number of nearest neighbours" << endl;
-		cin >> N;
 		s.clear();
-		kdtree *loc;
+		kdtree* loc;
 		 x = (int)(1e9) + 10;
 		set<pair<int, kdtree*> > ::iterator it;
-		nearest_neighbour(rootaddr, loc, x, xy, 0);
-		vector<kdtree*>vd;
+		if(i >= N){
+			//cout << "ho" << endl;
+		nearest_neighbour(rootaddr,loc, x, xy, 0);
+
+	}
+	//cout << s.size() << endl;
+ 		vector<kdtree*>vd;
 		for (it = s.begin(); it != s.end(); it ++) {
 			pair<int, kdtree*> y = *it;
 			vd.push_back(y.second);
 		}
+		//cout << s.size() << "^^" << endl;
+		for(int k = 0 ; k < min((int)(vd.size()),N); k ++){
+			for(int j = 0 ;j < DIM; j ++){
+				cout << vd[k]->di[j] << " ";
+			}
+			cout << endl;
+		}
 		vec.push_back(xy);
-		if ((i + 1) % blk) {
+		if ((i + 1) % blk == 0) {
 			delete_tree(rootaddr);
 			rootaddr = NULL;
 			rootaddr = static_build(rootaddr, vec, 0, 0, (int)vec.size() - 1);
